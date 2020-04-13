1b01b9b0eb48d241f439a59f56bb3a9029ffed7c
“Lomelinde” <celeblothc@gmail.com>
2020-04-13 17:11:03 +0800

    update

--- QuantumultX/Scripts/Wechat.js
+++ QuantumultX/Scripts/Wechat.js
@@ -1,13 +1,9 @@
 /*
 Remove the WeChat public account bottom ad
 
-QX:
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body Wechat.js
 
-Surge4：
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
-
-Surge & QX MITM = mp.weixin.qq.com,
+MITM = mp.weixin.qq.com
 */
 
 var obj = JSON.parse($response.body);
