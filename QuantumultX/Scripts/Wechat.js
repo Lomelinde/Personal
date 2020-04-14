be111517016eed5638bd5e2c64500431c769dcc6
“Lomelinde” <celeblothc@gmail.com>
2020-04-14 14:12:01 +0800

    update

--- QuantumultX/Scripts/Wechat.js
+++ QuantumultX/Scripts/Wechat.js
@@ -8,8 +8,10 @@ Remove the WeChat public account bottom ad
 hostname = mp.weixin.qq.com
 */
 
-var obj = JSON.parse($response.body);
-obj.advertisement_num = 0;
-obj.advertisement_info = [];
-delete obj.appid;
-$done({body: JSON.stringify(obj)}); 
+var data = {
+    body: "{}",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  $done({response: data});
